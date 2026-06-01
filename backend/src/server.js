99e7acbdfabbc925cba7df596f0b635fdda63c13
Cedric Urbino <cjurbino@gmail.com>
2026-06-01 16:19:01 -0500

    Linking PostgreSQL to the backend and SQL query executing properly

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const pool = require("./config/db");
 
 const app = express();
 
@@ -8,6 +9,25 @@ app.get("/", (req, res) => {
     status: "running",
     version: "0.1.0"
   });
+});
+
+app.get("/db-test", async (req, res) => {
+  try {
+    const result = await pool.query("SELECT NOW()");
+
+    res.json({
+      connected: true,
+      database_time: result.rows[0].now
+    });
+
+  } catch (error) {
+    console.error(error);
+
+    res.status(500).json({
+      connected: false,
+      error: error.message
+    });
+  }
 });
 
 app.listen(3000, () => {
