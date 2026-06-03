a54dbd3e7cc56f89244f262682c7727d707bd65e
Cedric Urbino <cjurbino@gmail.com>
2026-06-03 18:16:34 -0500

    Add packet retrieval endpoint

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,6 +3,8 @@ const pool = require("./config/db");
 
 const app = express();
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
   res.json({
     service: "Containerized Network Traffic Platform",
@@ -28,6 +30,67 @@ app.get("/db-test", async (req, res) => {
       error: error.message
     });
   }
+});
+
+app.post("/packets", async (req, res) => {
+  try {
+    const {
+      source_ip,
+      destination_ip,
+      protocol,
+      packet_length
+    } = req.body;
+
+    const result = await pool.query(
+      `
+      INSERT INTO packets
+      (source_ip, destination_ip, protocol, packet_length)
+      VALUES ($1, $2, $3, $4)
+      RETURNING *
+      `,
+      [
+        source_ip,
+        destination_ip,
+        protocol,
+        packet_length
+      ]
+    );
+
+    res.status(201).json(result.rows[0]);
+
+  } catch (error) {
+
+    console.error(error);
+
+    res.status(500).json({
+      error: error.message
+    });
+
+  }
+});
+
+app.get("/packets", async (req, res) => {
+
+  try {
+
+    const result = await pool.query(`
+      SELECT *
+      FROM packets
+      ORDER BY timestamp DESC
+    `);
+
+    res.json(result.rows);
+
+  } catch (error) {
+
+    console.error(error);
+
+    res.status(500).json({
+      error: error.message
+    });
+
+  }
+
 });
 
 app.listen(3000, () => {
